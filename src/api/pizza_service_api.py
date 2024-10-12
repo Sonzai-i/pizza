6e1757650b58ea4d35e93ff13d02c9b4a24132bc
Анна <sonzai_shi@inbox.ru>
2024-10-12 14:03:13 +0300

    реализовать REST API с помощью APIRouter из fast api

--- src/api/pizza_service_api.py
+++ src/api/pizza_service_api.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 
 from ..service.pizza_service import PizzaService
@@ -8,6 +8,7 @@ from ..model.entities import Pizza, OrderStatus
 from ..model.db import InMemDb
 
 app = FastAPI()
+router = APIRouter()
 
 db = InMemDb()
 pizza_service = PizzaService(db)
@@ -20,49 +21,51 @@ class PizzaModel(BaseModel):
 
     def to_pizza(self) -> Pizza:
         return Pizza(
-            pizza_id= self.pizza_id,
-            base_pizza_id= self.base_pizza_id,
-            topping_ids= self.topping_ids
+            pizza_id=self.pizza_id,
+            base_pizza_id=self.base_pizza_id,
+            topping_ids=self.topping_ids
         )
 
 
-
-@app.post("/order/")
+@router.post("/order/")
 async def create_order(user_id: str):
     return pizza_service.create_order(user_id=user_id)
 
 
-@app.get("/order/")
+@router.get("/order/")
 async def calc_price(order_id: str):
     return pizza_service.calc_price(order_id=order_id)
 
 
-@app.put("/order/payment/")
+@router.put("/order/payment/")
 async def on_payment_complete(order_id: str):
     return pizza_service.on_payment_complete(order_id=order_id)
 
 
-@app.put("/order/status/")
+@router.put("/order/status/")
 async def update_order_status(order_id: str, status: OrderStatus):
     return pizza_service.update_order_status(order_id=order_id, status=status)
 
 
-@app.post("/user/")
+@router.post("/user/")
 async def add_user(name: str, phone_number: int):
     return pizza_service.add_user(name=name, phone_number=phone_number)
 
 
-@app.put("/user/{order_id}")
+@router.post("/user/{order_id}")
 async def update_address(order_id: str, address: str):
     return pizza_service.update_address(order_id=order_id, address=address)
 
 
-@app.post("/pizza/")
+@router.post("/pizza/")
 async def add_pizza(order_id: str, pizza_model: PizzaModel):
     pizza = pizza_model.to_pizza()
     return pizza_service.add_pizza(order_id=order_id, pizza=pizza)
 
 
-@app.delete("/pizza/{pizza_id}")
+@router.delete("/pizza/{pizza_id}")
 async def remove_pizza(order_id: str, pizza_id: str):
     return pizza_service.remove_pizza(order_id=order_id, pizza_id=pizza_id)
+
+
+app.include_router(router)
